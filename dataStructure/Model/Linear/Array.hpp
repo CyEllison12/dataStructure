532899f7eeac1e7f5f2ac561dceae4276cc0b15f
Cy Ellison <dragonz2626@gmail.com>
2019-01-30 13:12:13 -0700

    created the constructor and copy constructor

--- dataStructure/Model/Linear/Array.hpp
+++ dataStructure/Model/Linear/Array.hpp
@@ -34,7 +34,28 @@ public:
     
     int getSize() const;
     Type getFromIndex(int index);
-    void setAtIndex(int index, Type data);	
+    void setAtIndex(int index, Type data);
+};
+
+template <class Type>
+Array<Type> :: Array(int size)
+{
+    assert(size > 0);
+    this->size = size;
+    
+    internalArray = new Type[size];
 }
 
+template <class Type>
+Array<Type> :: Array(const Array<Type> & toCopy)
+{
+    this-> = toCopy.getSize();
+    
+    internalArray = new Type[size];
+    
+    for(int index = 0; index < size; index++)
+    {
+        internalArray[index] = toCopy[index];
+    }
+}
 #endif /* Array_h */
