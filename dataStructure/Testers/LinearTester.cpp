cee756d07932275efbf254f994e873a57bd682a2
Cy Ellison <dragonz2626@gmail.com>
2019-02-13 13:04:47 -0700

    added file testers

---
+++ dataStructure/Testers/LinearTester.cpp
@@ -0,0 +1,12 @@
+//
+//  LinearTester.cpp
+//  dataStructure
+//
+//  Created by Ellison, Cy on 2/13/19.
+//  Copyright © 2019 Ellison, Cy. All rights reserved.
+//
+
+#include "LinearTester.hpp"
+
+
+
---
+++ dataStructure/Testers/LinearTester.hpp
@@ -0,0 +1,31 @@
+//
+//  LinearTester.hpp
+//  dataStructure
+//
+//  Created by Ellison, Cy on 2/13/19.
+//  Copyright © 2019 Ellison, Cy. All rights reserved.
+//
+
+#ifndef LinearTester_hpp
+#define LinearTester_hpp
+
+#include "../Controller/Tools/Timer.hpp"
+#include "../Controller/FileController.hpp"
+
+#include "../Model/Linear/LinkedList.hpp"
+#include "../Model/Linear/Stack.hpp"
+#include "../Model/Linear/Queue.hpp"
+#include "../Model/Linear/Array.hpp"
+
+#include <iostream>
+using namespace std;
+
+class Lineartester
+{
+public:
+    void testVsStl();
+};
+
+
+
+#endif /* LinearTester_hpp */
